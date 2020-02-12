de4fa41c2853e8b2a98ee93f803243983cd647ec
Nabi Ozberkman <nozberkm@ucsd.edu>
2020-02-11 19:01:00 -0800

    Init

---
+++ whoami/src/main/java/com/example/whoami/WhoamiBackend.java
@@ -0,0 +1,13 @@
+package com.example.whoami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoamiBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoamiBackend.class, args);
+	}
+
+}

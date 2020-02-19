460742f195fabc72f6617d02acd3a58df2a5ca10
Nabi Ozberkman <nozberkm@ucsd.edu>
2020-02-18 19:30:00 -0800

    Remove throwable from controller

--- whoami/src/main/java/com/example/whoami/ExceptionController.java
+++ whoami/src/main/java/com/example/whoami/ExceptionController.java
@@ -1,13 +1,12 @@
 package com.example.whoami;
 
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionController extends Throwable {
+public class ExceptionController {
     @ExceptionHandler(value = UserNameTakenException.class)
     public ResponseEntity<Object> userException(UserNameTakenException exception) {
         return new ResponseEntity<>("Username is not available.", HttpStatus.NOT_ACCEPTABLE);
